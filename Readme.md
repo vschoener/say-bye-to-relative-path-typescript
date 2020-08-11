0d1663cc3b212556ece0dda080dfec1300299d15
Vincent Schoener <vincent.schoener@gmail.com>
2020-08-11 10:33:11 +0200

    Add configs and Readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Setup TS project with Art!
+
+Follow this medium article: https://medium.com/@VincentSchoener/setup-typescript-project-with-art-1b9cfc4b91a6
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 
-import { GoogleService } from "./services/google.service";
-import { AwsService } from "./services/aws.service";
+import { GoogleService } from './services/google.service';
+import { AwsService } from './services/aws.service';
 
-console.log(new AwsService().log("test"));
-console.log(new GoogleService().call("test"));
+const user = {
+  name: 'john',
+  lastname: 'doe',
+};
+
+console.log(new AwsService().log('test'));
+console.log(new GoogleService().call('test'));
+console.log(user);
